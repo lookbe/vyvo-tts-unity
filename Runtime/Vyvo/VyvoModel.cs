d73498c3f9cb12c433e85997c4d5720e9d075713
lukman <bakhtiar.lukman@gmail.com>
2026-01-19 10:47:57 +0700

    update

--- Runtime/Vyvo/VyvoModel.cs
+++ Runtime/Vyvo/VyvoModel.cs
@@ -18,75 +18,18 @@ namespace VyvoTTS
         protected const int end_of_human = tokeniser_length + 4;
         protected const int pad_token = tokeniser_length + 7;
 
-        protected class VyvoPayload : CompletionPayload
+        public override int[] Tokenize(string prompt)
         {
-            public string Prompt;
-        }
-
-        public override void Prompt(string prompt)
-        {
-            if (string.IsNullOrEmpty(prompt))
-            {
-                return;
-            }
-
-            if (_llamaContext == IntPtr.Zero)
-            {
-                Debug.LogError("invalid context");
-                return;
-            }
-
-            if (_llamaModel == IntPtr.Zero)
-            {
-                Debug.LogError("model not loaded");
-                return;
-            }
-
-            if (status != LlamaCpp.ModelStatus.Ready)
-            {
-                Debug.LogError("invalid status");
-                return;
-            }
-
-            status = LlamaCpp.ModelStatus.Generate;
-            var payload = new VyvoPayload()
-            {
-                Prompt = prompt,
-
-                Temp = this.temperature,
-                TopK = this.topK,
-                TopP = this.topP,
-                MinP = this.minP,
-                RepeatPenalty = this.repeatPenalty,
-
-            };
-
-            RunBackground(payload, RunMayaPrompt);
-        }
-
-        void RunMayaPrompt(VyvoPayload inputPayload, CancellationToken cts)
-        {
-            int BOS_TOKEN = LlamaCpp.Native.llama_vocab_bos(_llamaVocab);
-            int[] prompt_token = Tokenize(inputPayload.Prompt);
+            int[] prompt_token = base.Tokenize(prompt);
 
             List<int> token_list = new List<int>();
+
             token_list.Add(start_of_human);
             token_list.AddRange(prompt_token);
             token_list.Add(end_of_text);
             token_list.Add(end_of_human);
 
-            var payload = new GenerationPayload()
-            {
-                Tokens = token_list.ToArray(),
-                Temp = inputPayload.Temp,
-                TopK = inputPayload.TopK,
-                TopP = inputPayload.TopP,
-                MinP = inputPayload.MinP,
-                RepeatPenalty = inputPayload.RepeatPenalty,
-
-            };
-
-            RunGenerate(payload, cts);
+            return token_list.ToArray();
         }
 
         protected override bool EndGeneration(int token, int generated_token_count)
